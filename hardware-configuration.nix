ffa46633a1387a77e3a0e5fb2b106866ffeaf636
Cole Mickens <cole.mickens@gmail.com>
2020-10-25 16:55:02 -0700

    small fixup

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -14,7 +14,7 @@
 
         "${modulesPath}/installer/scan/not-detected.nix"
 
-    ]
+    ];
     boot.loader.systemd-boot.enable = true; # (for UEFI systems only)
     fileSystems."/".device = "/dev/disk/by-label/nixos";
 }
